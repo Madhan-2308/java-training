b123f1a9d81dd2a69d1489e3e81cb64876eab9d8
Madhan S <madantheconquer@gmail.com>
2026-07-03 12:07:41 +0530

    Day3

---
+++ Day2/Problem2.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class Problem2 {
+    public static void main() {
+        String str1 = "hello";
+        String str2 = "world";
+
+        System.out.println(str1.compareTo(str2));
+
+    }
+    }
+    
